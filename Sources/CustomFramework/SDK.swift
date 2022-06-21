31ae29436167fe09b3ce0e03b36cc9d0e0a04128
Pedro José Blaya Cano <blayaburgo@gmail.com>
2022-06-21 17:03:19 +0200

    Initial Commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,15 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Pedro José Blaya Cano on 21/6/22.
+//
+
+import Foundation
+
+public class SDK {
+    
+    public static func doSomeWork() {
+        print("Hacemos algo ...")
+    }
+}
